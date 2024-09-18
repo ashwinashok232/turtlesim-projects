875dabee966e464e14257d106c0273a305c3e92b
Ashwin Ashok <ashwinashok232@gmail.com>
2024-09-17 20:42:35 -0700

    Add spawn kill functionality

--- turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
+++ turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
@@ -39,10 +39,11 @@ Some optional arguments you can add:
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist 
-from turtlesim_interfaces.msg import SpawnList
-from geometry_msgs.msg import Vector3
+from turtlesim_interfaces.msg import SpawnInfo
 from turtlesim.msg import Pose
 import math
+from turtlesim.srv import Kill
+from functools import partial
  
 class TurtleControllerNode(Node): #MODIFY
     def __init__(self):
@@ -51,29 +52,35 @@ class TurtleControllerNode(Node): #MODIFY
         self.turtle_pose_ = (0.0, 0.0)
         self.goalseeking_ = False
         self.target_pose_ = None
+        self.target_name_ = None
         # self.publisher_ = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
         self.main_turtle_pose_subscriber_ = self.create_subscription(Pose, "turtle1/pose", self.pose_callback, 10)
-        self.spawn_coordinate_subscriber_ = self.create_subscription(Vector3, "spawn_coordinates", self.spawn_coordinate_callback, 10)
+        self.spawn_coordinate_subscriber_ = self.create_subscription(SpawnInfo, "spawn_coordinates", self.spawn_coordinate_callback, 10)
         self.velocity_publisher_ = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
         # self.timer_ = self.create_timer(0.1,self.velocity_callback)
         self.get_logger().info("Controller Node started")
 
     def spawn_coordinate_callback(self, msg):
         self.get_logger().info("LOGGING COORDINATES")
-        self.spawn_coordinate_list_.append([msg.x, msg.y])
+        # self.spawn_coordinate_list_.append([msg.x, msg.y])
+        self.spawn_coordinate_list_.append({"x": msg.x_coord, "y": msg.y_coord, "name": msg.name})
         turtle_coord = [self.turtle_pose_[0], self.turtle_pose_[1]]
         self.get_logger().info(str(self.spawn_coordinate_list_))
         if not self.goalseeking_:
             smallestDist = math.inf
             targetPoint = None
-            for spawn_coord in self.spawn_coordinate_list_:
+            spawnName = None
+            for spawn_dict in self.spawn_coordinate_list_:
+                spawn_coord = [spawn_dict["x"], spawn_dict["y"]]
                 self.get_logger().info(str(spawn_coord) + " " + str(turtle_coord))
                 currentDist = math.dist(spawn_coord, turtle_coord)
                 if currentDist < smallestDist:
                     smallestDist = currentDist
                     targetPoint = spawn_coord
+                    spawnName = spawn_dict["name"]
                     # self.get_logger().info("CALCULATING TARGET: " + str(smallestDist) + " " + str(targetPoint))
             self.target_pose_ = targetPoint
+            self.target_name_ = spawnName
             self.goalseeking_ = True
             # self.get_logger().info("FINAL CLOSEST POINT:" + str(smallestDist) + " " + str(targetPoint))
             
@@ -84,31 +91,52 @@ class TurtleControllerNode(Node): #MODIFY
         self.velocity_callback()
         
     def velocity_callback(self):
-        linear_gain = 0.4
-        angular_gain = 0.8
+        linear_gain = 1.0
+        angular_gain = 1.5
         goal_tol = 0.1
 
         if self.goalseeking_:
             polar_distance_error = math.dist(self.target_pose_, [self.turtle_pose_[0], self.turtle_pose_[1]])
             polar_angle_error = convertPrincipalToFullCircleAngle(math.atan2(self.target_pose_[1]-self.turtle_pose_[1], 
                                            self.target_pose_[0]-self.turtle_pose_[0])) - convertPrincipalToFullCircleAngle(self.turtle_pose_[2])
-            if (polar_distance_error <= 0.1):
+            if (polar_distance_error <= goal_tol):
                 msg = Twist()
                 msg.linear.x = 0.0
                 msg.angular.z = 0.0
                 msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y = 0.0, 0.0, 0.0, 0.0
                 self.velocity_publisher_.publish(msg)
-                self.spawn_coordinate_list_.remove(self.target_pose_)
+                self.spawn_coordinate_list_.remove({"x": self.target_pose_[0], "y": self.target_pose_[1], "name": self.target_name_})
+                self.call_kill_service(self.target_name_)
                 self.goalseeking_ = False
                 self.get_logger().info('################## GOAL REACHED ##################')
             # while self.target_pose_ != [self.turtle_pose_[0], self.turtle_pose_[1]]:
             # self.get_logger().info(f"DISTANCE ERROR: {polar_distance_error}   ANGLE ERROR: {polar_angle_error}")
             # self.get_logger().info(f"{math.atan2(self.target_pose_[1]-self.turtle_pose_[1], self.target_pose_[0]-self.turtle_pose_[0])}         {self.turtle_pose_[2]}")
-            msg = Twist()
-            msg.linear.x = linear_gain*polar_distance_error
-            msg.angular.z = angular_gain*polar_angle_error
-            msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y = 0.0, 0.0, 0.0, 0.0
-            self.velocity_publisher_.publish(msg)
+            else:
+                msg = Twist()
+                msg.linear.x = linear_gain*polar_distance_error
+                msg.angular.z = angular_gain*polar_angle_error
+                msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y = 0.0, 0.0, 0.0, 0.0
+                self.velocity_publisher_.publish(msg)
+            
+    def call_kill_service(self, name):        
+        client = self.create_client(Kill, "kill")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for Kill Server...")
+
+        #SAMPLE
+        request = Kill.Request()
+        request.name = name
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_call_kill_service, name=name))
+
+    def callback_call_kill_service(self, future, name):
+        try:
+            response = future.result()
+            self.get_logger().info(f"Killing {name}")
+        except Exception as e:
+            self.get_logger().error("Service call failed %r" % (e,))
 
 def main(args=None):
     rclpy.init(args=args)
--- turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -4,8 +4,7 @@ from rclpy.node import Node
 from functools import partial
 
 from turtlesim.srv import Spawn
-from turtlesim_interfaces.msg import SpawnList
-from geometry_msgs.msg import Vector3
+from turtlesim_interfaces.msg import SpawnInfo
 import random
 
 # <service_type> Type of service message eg: AddTwoInts (from example_interfaces.srv)
@@ -15,37 +14,37 @@ class SpawnTurtleNode(Node):
     def __init__(self):
         super().__init__("turtle_spawner")
         self.spawnCoordinateList_ = []
-        self.create_timer(4.0,self.turtle_spawn_timer_callback)
-        self.publisher_ = self.create_publisher(Vector3, "spawn_coordinates", 10)
-        # self.call_spawn_service(3.0, 2.0, 0.0)
+        self.spawn_count_ = 0
+        self.create_timer(3.0,self.turtle_spawn_timer_callback)
+        self.publisher_ = self.create_publisher(SpawnInfo, "spawn_coordinates", 10)
 
-    def call_spawn_service(self, x, y, theta):        
+    def call_spawn_service(self, x, y, theta, name):        
         client = self.create_client(Spawn, "spawn")
         while not client.wait_for_service(1.0):
             self.get_logger().warn("Waiting for Spawn Server...")
 
-        #SAMPLE
         request = Spawn.Request()
         request.x = x
         request.y = y
         request.theta = theta
+        request.name = name
 
         # We prefer to use async since it prevents the whole system from stopping
         future = client.call_async(request)
-        future.add_done_callback(partial(self.callback_call_spawn_service, x=x, y=y, theta=theta))
+        future.add_done_callback(partial(self.callback_call_spawn_service, x=x, y=y, theta=theta, name=name))
 
-    def callback_call_spawn_service(self, future, x, y, theta):
+    def callback_call_spawn_service(self, future, x, y, theta, name):
         try:
             response = future.result()
-            self.get_logger().info("x = " + str(x) + "\ny = " + str(y) + "\ntheta = " + str(theta))
-            coord = Vector3()
-            coord.x = x
-            coord.y = y
-            coord.z = 0.0
-            # self.spawnCoordinateList_.append(coord)
-            # msg = SpawnList()
-            # msg.coordinates = self.spawnCoordinateList_
-            self.publisher_.publish(coord)
+            self.get_logger().info("x = " + str(x) + "\ny = " + str(y) + "\ntheta = " + str(theta) + "\nname = " + name)
+            msg = SpawnInfo()
+            msg.x_coord = x
+            msg.y_coord = y
+            msg.theta = theta
+            msg.name = name
+            self.publisher_.publish(msg)
+            self.spawn_count_ += 1
+            
         except Exception as e:
             self.get_logger().error("Service call failed %r" % (e,))
 
@@ -54,12 +53,11 @@ class SpawnTurtleNode(Node):
         
         x_spawn = round(random.uniform(x_min,x_max),1)
         y_spawn = round(random.uniform(y_min,y_max),1)
-        # self.get_logger().info(f"({x_spawn},{y_spawn})")
-        self.call_spawn_service(x_spawn, y_spawn, 0.0)
+        self.call_spawn_service(x_spawn, y_spawn, 0.0, f"Spawn{self.spawn_count_}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = SpawnTurtleNode() #MODIFY
+    node = SpawnTurtleNode()
     rclpy.spin(node)
     rclpy.shutdown()
  
